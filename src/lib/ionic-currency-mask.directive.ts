57c5da8eaa49ca5645bb25e54e9c6c1961930b61
Thiago Przyczynski <przyczynski@gmail.com>
2021-05-19 09:03:04 -0300

    Fixed identation

--- src/lib/ionic-currency-mask.directive.ts
+++ src/lib/ionic-currency-mask.directive.ts
@@ -1,5 +1,5 @@
 import {Attribute, Directive} from '@angular/core';
-import { NgModel } from '@angular/forms';
+import {NgModel} from '@angular/forms';
 
 @Directive({
   selector: '[currencyMask]',
@@ -41,20 +41,20 @@ export class CurrencyMaskDirective {
    * @param event
    */
   changeValue(event: any) {
-	let value = event.target.value;
-	if (value == '') {
-		return;
-	}
-	value = value + '';
-	value = parseInt(value.replace(/[\D]+/g, ''));
-	value = value + '';
-	value = value.replace(/([0-9]{2})$/g, this.decimal + '$1');
-	var parts = value.toString().split(this.decimal);
-	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, this.thousand);
-	value = parts.join(this.decimal);
-	console.log(value);
-	event.target.value = value;
-	this.model.update.emit(value);
-	return true;
+    let value = event.target.value;
+    if (value == '') {
+      return;
+    }
+    value = value + '';
+    value = parseInt(value.replace(/[\D]+/g, ''));
+    value = value + '';
+    value = value.replace(/([0-9]{2})$/g, this.decimal + '$1');
+    var parts = value.toString().split(this.decimal);
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, this.thousand);
+    value = parts.join(this.decimal);
+    console.log(value);
+    event.target.value = value;
+    this.model.update.emit(value);
+    return true;
   }
 }
